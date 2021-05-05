55b8226d944132f9cf15e58efb82b85cb287f8f6
YoloSan <veselu@gmail.com>
2021-05-05 22:19:30 +0300

    finish state&getters

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,35 @@ export default createStore({
       "food",
       "community",
     ],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false },
+    ],
+    events: [
+      { id: 1, text: "...", organizer: "..." },
+      { id: 2, text: "...", organizer: "..." },
+      { id: 3, text: "...", organizer: "..." },
+      { id: 4, text: "...", organizer: "..." },
+    ],
+  },
+  getters: {
+    catLength: (state) => {
+      return state.categories.length;
+    },
+    doneTodos: (state) => {
+      return state.todos.filter((todo) => todo.done);
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length;
+    },
+    activeTodosCount2: (state) => {
+      return state.todos.filter((todo) => !todo.done).length;
+    },
+    getEventById: (state) => (id) => {
+      return state.events.find((event) => event.id === id);
+    },
   },
   mutations: {},
   actions: {},
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     <h1>Create an Event, {{ userName }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="category of categs" :key="category">
+        {{ category }}
+      </li>
+    </ul>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEvent(1) }}</p>
+    <p>{{ getEventById(2) }}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState } from "vuex";
+
 export default {
   name: "EventCreate",
 
@@ -12,6 +23,19 @@ export default {
     userName() {
       return this.$store.state.user.name;
     },
+    catLength() {
+      return this.$store.getters.catLength;
+    },
+    ...mapState({
+      userID: (state) => state.user.id,
+      // can be used for top level state
+      categs: "categories",
+    }),
+    ...mapState(["user", "categories"]),
+    getEvent() {
+      return this.$store.getters.getEventById;
+    },
+    ...mapGetters(["getEventById"]),
   },
 };
 </script>
