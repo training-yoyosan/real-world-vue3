a473b3eb102a681041576655375abf52637b9d1c
YoloSan <veselu@gmail.com>
2021-05-10 22:23:06 +0300

    mutations&actions #2: avoid extra ajax call

--- src/store/index.js
+++ src/store/index.js
@@ -70,14 +70,20 @@ export default createStore({
           console.log("There was an error:" + error.response);
         });
     },
-    fetchEvent({ commit }, id) {
-      EventService.getEvent(id)
-        .then((response) => {
-          commit("SET_EVENT", response.data);
-        })
-        .catch((error) => {
-          console.log("There was an error:", error.response);
-        });
+    fetchEvent({ commit, getters }, id) {
+      const event = getters.getEventById(id);
+
+      if (event) {
+        commit("SET_EVENT", event);
+      } else {
+        EventService.getEvent(id)
+          .then((response) => {
+            commit("SET_EVENT", response.data);
+          })
+          .catch((error) => {
+            console.log("There was an error:", error.response);
+          });
+      }
     },
   },
   modules: {},
